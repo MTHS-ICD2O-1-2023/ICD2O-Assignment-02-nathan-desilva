88b471fa46d4057f1b13542bb8045151a19e3c18
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-03-24 23:09:58 +0000

    Added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Nathan De Silva All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Nathan De Silva
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+/**
+ * This function gets users street number and street name and shows it back to user.
+ */
+function doMathClicked() {
+  // input
+  const length = parseInt(document.getElementById("length").value)
+  const width = parseInt(document.getElementById("width").value)
+  const height = parseInt(document.getElementById("height").value)
+  // output
+  document.getElementById("user-info").innerHTML =
+    "<p>The volume of a rectangular prism is: " + length * width * height + "cm³. </p>"
+}
